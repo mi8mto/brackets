912fb4cf4741877ee08e51c4012ab98422ffe922
mi8mto <95370082+mi8mto@users.noreply.github.com>
2023-01-17 14:48:27 +0300

    fix: fixed

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
   // your solution (oldest)
   const event = bracketsConfig.map((i) => i.join(""));
   for (let i = 0; i < event.length; i++) {
-    if (str.includes(arr[i])) {
+    if (str.includes(event[i])) {
       str = str.replaceAll(event[i], "");
       i = -1;
     }
